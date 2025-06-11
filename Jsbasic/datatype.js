d7d2738b9e220ddf5b913029902abbed8f387e66
Danger-Smile <worldknowledgeacademy43@gmail.com>
2025-06-11 15:28:13 +0000

    datatype

---
+++ Jsbasic/datatype.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all js code as newer version
+
+// alert("not allowed")// not allowed because we using nodejs
+
+
+
+let a
+console.log(a)  
+
+// null is the representation of empty value 
+
+let Null=null
+console.log(Null)
+
+// symbol is used to unique  like unique component
+
+console.log(typeof 6)
+console.log(typeof null)
+console.log(typeof undefined)
